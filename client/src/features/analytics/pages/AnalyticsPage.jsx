b33e315a8aded8a45aca9fdc2c3be6ebec65e293
Deepthi-M555 <mdeepthi555@gmail.com>
2026-05-28 15:17:55 +0530

    Created analytics overview page

---
+++ client/src/features/analytics/pages/AnalyticsPage.jsx
@@ -0,0 +1,20 @@
+import MainLayout
+from "../../../components/layout/MainLayout";
+
+function AnalyticsPage() {
+
+  return (
+
+    <MainLayout>
+
+      <h1>
+        Analytics
+      </h1>
+
+    </MainLayout>
+
+  );
+
+}
+
+export default AnalyticsPage;
