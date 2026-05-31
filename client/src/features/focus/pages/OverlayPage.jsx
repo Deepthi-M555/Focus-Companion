be12039261fd533416ccf6ef85fa357caf022910
Deepthi-M555 <mdeepthi555@gmail.com>
2026-06-01 04:35:26 +0530

    Added focus overlay interface

---
+++ client/src/features/focus/pages/OverlayPage.jsx
@@ -0,0 +1,21 @@
+function OverlayPage() {
+
+  return (
+
+    <div className="overlay">
+
+      <h2>
+        Focus Mode
+      </h2>
+
+      <p>
+        25:00
+      </p>
+
+    </div>
+
+  );
+
+}
+
+export default OverlayPage;
