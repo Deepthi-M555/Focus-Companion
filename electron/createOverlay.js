7865c0d34b951d393789b8d8091460c43f1d2d4a
Deepthi-M555 <mdeepthi555@gmail.com>
2026-06-17 10:02:17 +0530

    Implemented Electron overlay window

---
+++ electron/createOverlay.js
@@ -0,0 +1,56 @@
+const {
+    BrowserWindow
+} = require("electron");
+
+const path = require("path");
+
+function createOverlay() {
+
+    const overlay =
+        new BrowserWindow({
+
+            width: 320,
+
+            height: 180,
+
+            frame: false,
+
+            transparent: true,
+
+            alwaysOnTop: true,
+
+            skipTaskbar: true,
+
+            resizable: false,
+
+            show: false,
+
+            webPreferences: {
+
+                preload:
+                    path.join(
+                        __dirname,
+                        "preload.js"
+                    ),
+
+                nodeIntegration:
+                    false,
+
+                contextIsolation:
+                    true,
+
+                sandbox:
+                    true
+
+            }
+
+        });
+
+    overlay.loadURL(
+        "http://localhost:5173/overlay"
+    );
+
+    return overlay;
+}
+
+module.exports = createOverlay;
