eca77dac9c5006990e565a761ce8136713559eaa
Deepthi-M555 <mdeepthi555@gmail.com>
2026-06-17 10:02:08 +0530

    Added Electron main window factory

---
+++ electron/createMainWindow.js
@@ -0,0 +1,45 @@
+const {
+    BrowserWindow
+} = require("electron");
+
+const path = require("path");
+
+function createMainWindow() {
+
+    const win =
+        new BrowserWindow({
+
+            width: 1400,
+
+            height: 900,
+
+            webPreferences: {
+
+                preload:
+                    path.join(
+                        __dirname,
+                        "preload.js"
+                    ),
+
+                nodeIntegration:
+                    false,
+
+                contextIsolation:
+                    true,
+
+                sandbox:
+                    true
+
+            }
+
+        });
+
+    win.loadURL(
+        "http://localhost:5173"
+    );
+
+    return win;
+}
+
+module.exports =
+    createMainWindow;
