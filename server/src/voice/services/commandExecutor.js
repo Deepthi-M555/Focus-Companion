614488600ccad5c6f8798cdd7021b7312d5b479f
Deepthi-M555 <mdeepthi555@gmail.com>
2026-06-18 06:03:57 +0530

    Added voice command executor service

---
+++ server/src/voice/services/commandExecutor.js
@@ -0,0 +1,58 @@
+async function executeCommand({
+
+    intent,
+
+    duration,
+
+    socket,
+
+    sessionId
+
+}) {
+
+    switch (intent) {
+
+        case "PAUSE_SESSION":
+
+            socket.emit(
+                "pause-session",
+                { sessionId }
+            );
+
+            break;
+
+        case "RESUME_SESSION":
+
+            socket.emit(
+                "resume-session",
+                { sessionId }
+            );
+
+            break;
+
+        case "COMPLETE_SESSION":
+
+            socket.emit(
+                "complete-session",
+                { sessionId }
+            );
+
+            break;
+
+        case "SNOOZE_SESSION":
+
+            socket.emit(
+                "snooze-session",
+                {
+                    sessionId,
+                    duration
+                }
+            );
+
+            break;
+    }
+}
+
+module.exports = {
+    executeCommand
+};
