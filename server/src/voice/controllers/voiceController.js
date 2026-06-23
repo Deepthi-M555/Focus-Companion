e018ee55a9df92b29e9fdbd4a113d7e4e59e1e0b
Deepthi-M555 <mdeepthi555@gmail.com>
2026-06-23 22:11:34 +0530

    Enhanced voice controller functionality

--- server/src/voice/controllers/voiceController.js
+++ server/src/voice/controllers/voiceController.js
@@ -1,20 +1,89 @@
 const {
-    detectIntent
+    detectIntent,
+    executeIntent
 } = require(
-    "../services/intentService"
+    "../../services/intentService"
 );
 
-exports.processVoice =
-async (req, res) => {
+const {
+    STATES
+} = require(
+    "../../services/focusStateMachine"
+);
 
-    const {
-        transcript
-    } = req.body;
+exports.voiceCheckIn =
+    async (
+        req,
+        res
+    ) => {
 
-    const intent =
-        detectIntent(
-            transcript
-        );
+        try {
 
-    res.json(intent);
-};
+            const {
+                transcript
+            } = req.body;
+
+            if (
+                !transcript
+            ) {
+
+                return res
+                    .status(400)
+                    .json({
+
+                        success: false,
+
+                        error: {
+                            message:
+                                "Transcript is required."
+                        }
+
+                    });
+
+            }
+
+            const intent =
+                detectIntent(
+                    transcript
+                );
+
+            const result =
+                executeIntent({
+
+                    intent,
+
+                    currentState:
+                        STATES.CHECK_IN_PENDING
+
+                });
+
+            return res.json({
+
+                success: true,
+
+                transcript,
+
+                intent,
+
+                ...result
+
+            });
+
+        } catch (error) {
+
+            return res
+                .status(500)
+                .json({
+
+                    success: false,
+
+                    error: {
+                        message:
+                            error.message
+                    }
+
+                });
+
+        }
+
+    };
