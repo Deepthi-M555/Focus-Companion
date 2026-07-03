4aee0dd1aa2ceb778f864b84c30ce772efcef0ca
Deepthi-M555 <mdeepthi555@gmail.com>
2026-07-03 22:03:57 +0530

    Add frontend task service integration

---
+++ clients/src/app/services/taskService.js
@@ -0,0 +1,35 @@
+import api from "./api";
+
+export async function saveSchedule(
+
+    data
+
+){
+
+    const response =
+
+        await api.post(
+
+            "/tasks/save",
+
+            data
+
+        );
+
+    return response.data;
+
+}
+
+export async function loadTodaySchedule(){
+
+    const response =
+
+        await api.get(
+
+            "/tasks/today"
+
+        );
+
+    return response.data;
+
+}
