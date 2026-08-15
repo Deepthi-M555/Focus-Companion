3f9433e597705370797b2d40cf7321a12c509809
Deepthi-M555 <mdeepthi555@gmail.com>
2026-08-15 23:30:24 +0530

    feat: improve frontend task service

--- client/src/app/services/taskService.js
+++ client/src/app/services/taskService.js
@@ -1,18 +1,18 @@
 import api from "./api";
 
 export async function saveSchedule(data) {
-  const response = await api.post(
-    "/tasks/save",
-    data
-  );
+    const response = await api.post(
+        "/tasks/save",
+        data
+    );
 
-  return response.data;
+    return response.data;
 }
 
-export async function loadTodaySchedule() {
-  const response = await api.get(
-    "/tasks/today"
-  );
+export async function loadActiveSchedule() {
+    const response = await api.get(
+        "/tasks/today"
+    );
 
-  return response.data;
+    return response.data;
 }
