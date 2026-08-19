e28f9e553c43ac097dbff15f4c0480c115998191
Deepthi-M555 <mdeepthi555@gmail.com>
2026-08-19 10:08:02 +0530

    feat: update authentication routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,7 +3,23 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const authController = require("../controllers/authController");
 
+const redisRateLimit =
+    require(
+        "../middleware/redisRateLimit"
+    );
+    
 router.post("/signup", wrapAsync(authController.signup));
-router.post("/login", wrapAsync(authController.login));
+router.post(
+    "/login",
 
+    redisRateLimit({
+        windowSeconds: 60,
+        maxRequests: 10,
+        keyPrefix: "login"
+    }),
+
+    wrapAsync(
+        authController.login
+    )
+);
 module.exports = router;
