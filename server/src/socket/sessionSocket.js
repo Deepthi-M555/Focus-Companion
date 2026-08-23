f18bada7d04be35928c5f0432800fdd7e6398958
Deepthi-M555 <mdeepthi555@gmail.com>
2026-08-23 21:52:40 +0530

    fix: handle voice responses through session socket

--- server/src/socket/sessionSocket.js
+++ server/src/socket/sessionSocket.js
@@ -1,24 +1,9 @@
-const {
-
-    STATES,
-
-    transition
-
-} = require(
-    "../services/focusStateMachine"
-);
-
 module.exports = (
     io,
     socket
 ) => {
 
-    /*
-        Join Session Room
-    */
-
     socket.on(
-
         "join_focus_session",
 
         (sessionId) => {
@@ -28,175 +13,10 @@ module.exports = (
             );
 
             console.log(
-
-                `Joined session:
-
-                ${sessionId}`
-
+                `Joined session: ${sessionId}`
             );
 
         }
-
-    );
-
-    /*
-        Voice Check-In Response
-    */
-
-    socket.on(
-
-        "voice-response",
-
-        ({
-
-            sessionId,
-
-            transcript,
-
-            currentState
-
-        }) => {
-
-            const text =
-                transcript
-                .toLowerCase();
-
-            try {
-
-                /*
-                    YES
-                */
-
-                if (
-
-                    text.includes(
-                        "yes"
-                    ) ||
-
-                    text.includes(
-                        "completed"
-                    )
-
-                ) {
-
-                    const nextState =
-                        transition({
-
-                            currentState,
-
-                            nextState:
-                                STATES.COMPLETED
-
-                        });
-
-                    io.to(sessionId)
-
-                        .emit(
-
-                            "session-state",
-
-                            {
-
-                                state:
-                                    nextState
-
-                            }
-
-                        );
-
-                    return;
-                }
-
-                /*
-                    SNOOZE
-                */
-
-                if (
-
-                    text.includes(
-                        "snooze"
-                    )
-
-                ) {
-
-                    const nextState =
-                        transition({
-
-                            currentState,
-
-                            nextState:
-                                STATES.SNOOZED
-
-                        });
-
-                    io.to(sessionId)
-
-                        .emit(
-
-                            "session-state",
-
-                            {
-
-                                state:
-                                    nextState
-
-                            }
-
-                        );
-
-                    return;
-                }
-
-                /*
-                    NEED HELP
-                */
-
-                const nextState =
-                    transition({
-
-                        currentState,
-
-                        nextState:
-                            STATES.RECOVERY_ENGINE
-
-                    });
-
-                io.to(sessionId)
-
-                    .emit(
-
-                        "session-state",
-
-                        {
-
-                            state:
-                                nextState
-
-                        }
-
-                    );
-
-            }
-
-            catch (error) {
-
-                socket.emit(
-
-                    "session-error",
-
-                    {
-
-                        message:
-                            error.message
-
-                    }
-
-                );
-
-            }
-
-        }
-
     );
 
 };
